81debaef86331ac432af72958d4358889acd647b
Jean Rodriguez <jeanrodriguez24@gmail.com>
2012-06-14 14:07:02 -0400

    Added redis for session store.

--- app.js
+++ app.js
@@ -4,16 +4,29 @@
  */
 
 var express = require('express');
+var RedisStore = require('connect-redis')(express);
 require('express-namespace');
 var mongoose = require('mongoose');
 var helpers = require('./helpers');
 var i18n = require('i18n');
 var publicDir = __dirname + '/public';
 var port;
+var redisClient;
 
 var app = module.exports = express.createServer();
 app.__i = i18n.__;
 app.__n = i18n.__n;
+
+// Heroku redistogo connection
+app.configure('production', function () {
+    var rtg = require('url').parse(process.env.REDISTOGO_URL);
+    redisClient = require('redis').createClient(rtg.port, rtg.hostname);
+    redisClient.auth(rtg.auth.split(':')[1]);
+});
+
+app.configure('development', function () {
+    redisClient = require("redis").createClient();
+});
 
 // Configuration
 app.configure(function () {
@@ -21,7 +34,11 @@ app.configure(function () {
     app.set('view engine', 'jade');
     app.use(express.bodyParser());      
     app.use(express.cookieParser());
-    app.use(express.session({ secret: "AwesomeDepotApp" }));
+    app.use(express.session({
+        secret: process.env.CLIENT_SECRET || "AwesomeDepotApp",
+        maxAge: new Date().now + 7200000,
+        store: new RedisStore({ client: redisClient })
+    }));
     app.use(express.methodOverride());
     app.use(app.router);
     app.use(express['static'](publicDir));
@@ -43,7 +60,7 @@ app.configure('development', function () {
 app.configure('production', function () { 
     app.use(express.errorHandler()); 
     app.set(process.env.MONGOLAB_URI);
-    port = process.env.PORT;
+    port = process.env.PORT;    
 });
 
 // Routes
